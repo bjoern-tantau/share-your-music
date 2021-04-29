8941af035073b9b61ab8666d165099c49d9a2d14
Björn Tantau <bjoern@bjoern-tantau.de>
2021-04-29 15:48:15 +0200

    Add support for ranges.
    fixes #1

--- public/download.php
+++ public/download.php
@@ -6,17 +6,22 @@ use M3uParser\M3uParser;
 
 $config = include __DIR__ . '/../config/config.php';
 
-$response = new Response();
+$request  = (new Jasny\HttpMessage\ServerRequest())->withGlobalEnvironment();
+$response = (new Response())
+    ->withGlobalEnvironment()
+    ->withHeader('Accept-Ranges', 'bytes')
+;
+$emitter  = new Laminas\HttpHandlerRunner\Emitter\SapiStreamEmitter();
 
 $uploadDir     = realpath(__DIR__ . '/../uploads');
 $requestedPath = urldecode($_SERVER['REQUEST_URI']);
 $path          = realpath($uploadDir . $requestedPath);
 
 if (strpos($path, $uploadDir) !== 0 || !is_file($path) || !is_readable($path)) {
-    $response
-        ->withStatus(404)
-        ->emit()
-    ;
+    $emitter->emit(
+        $response
+            ->withStatus(404)
+    );
     exit;
 }
 
@@ -28,15 +33,59 @@ if (str_ends_with($path, '.m3u')) {
         $files[] = dirname($requestedPath) . '/' . $musicFile->getPath();
     }
     $response->getBody()->write(json_encode($files));
-    $response
-        ->withHeader('Content-Type', 'application/json')
-        ->emit()
-    ;
+    $emitter->emit(
+        $response
+            ->withHeader('Content-Type', 'application/json')
+    );
     exit;
 }
 
-$response
-    ->withHeader('Content-Type', mime_content_type($path))
-    ->withBody(Stream::open($path, 'r'))
-    ->emit()
-;
+$body = Stream::open($path, 'r');
+
+$range = $request->getHeaderLine('Range');
+if (!empty($range)) {
+    $length = $body->getSize();
+    if (!preg_match('/^bytes=\d*-\d*(,\d*-\d*)*$/', $range)) {
+        $emitter->emit(
+            $response
+                ->withStatus(416)
+                ->withHeader('Content-Range', 'bytes */' . $length)
+        );
+        exit;
+    }
+    $ranges      = explode(',', substr($range, 6));
+    $contentFrom = $length - 1;
+    $contentTo   = 0;
+
+    foreach ($ranges as $range) {
+        $parts = explode('-', $range);
+        $from  = $parts[0];
+        $to    = $parts[1];
+        if (empty($to)) {
+            $contentTo = $length - 1;
+        }
+        if (empty($from)) {
+            $contentFrom = $length - $contentTo;
+        }
+        $contentFrom = $contentFrom < $from ? $contentFrom : $from;
+        $contentTo   = $contentTo > $to ? $contentTo : $to;
+    }
+
+    if ($contentTo >= $length || $contentFrom < 0) {
+        $emitter->emit(
+            $response
+                ->withStatus(416)
+                ->withHeader('Content-Range', 'bytes */' . $length)
+        );
+        exit;
+    }
+    $response = $response
+        ->withHeader('Content-Range', "bytes $contentFrom-$contentTo/$length")
+        ->withStatus(206)
+    ;
+}
+$emitter->emit(
+    $response
+        ->withHeader('Content-Type', mime_content_type($path))
+        ->withBody($body)
+);
